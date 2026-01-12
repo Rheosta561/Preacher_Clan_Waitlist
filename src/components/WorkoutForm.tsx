d0350d210e9a64e8db36f4a0d6a8c2e4cecacfce
Anubhav Mishra <manubhav731@gmail.com>
2026-01-12 22:34:18 +0530

    done

--- src/components/WorkoutForm.tsx
+++ src/components/WorkoutForm.tsx
@@ -19,7 +19,7 @@ export default function WorkoutForm({
     <div className="space-y-4">
       <Input type="file" accept="image/*" onChange={(e) => setImage(e.target.files?.[0])} />
 
-      {workout.map((ex: any, i: number) => (
+      {workout.map(( i: number) => (
         <div key={i} className="flex gap-2">
           <Input
             placeholder="Exercise"
